2185d6edc89c615abb882e2d5f5bfad75b84569a
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-05-28 11:08:46 +0000

    add extra pages and fill out about and faq pages

--- app.py
+++ app.py
@@ -5,9 +5,53 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# --------------------------HOMEPAGE------------------------- #
+
+
 @app.route("/")
-def index():
+@app.route("/homepage")
+def homepage():
     return render_template("index.html")
+
+
+# --------------------------BOOKS---------------------------- #
+
+
+@app.route("/books")
+def books():
+    return render_template("books.html")
+
+
+# --------------------------ABOUT---------------------------- #
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# --------------------------FAQ------------------------------ #
+
+
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
+
+# --------------------------SIGNUP-------------------------- #
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+# --------------------------CONTACT------------------------- #
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 # Don't forget to put debug in if devvelopment var
