0a17e496be9f0a5fb1f9234687cd9b595f12bb6b
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-06-01 11:02:32 +0000

    add books to book page

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 # --------------------------HOMEPAGE------------------------- #
@@ -21,7 +32,8 @@ def homepage():
 
 @app.route("/books")
 def books():
-    return render_template("books.html")
+    books = mongo.db.books.find()
+    return render_template("books.html", books=books)
 
 
 # --------------------------ABOUT---------------------------- #
