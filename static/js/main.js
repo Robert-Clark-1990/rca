ed66fc113ff1e38787bc296bc41b700bea9dad0e
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-06-02 10:44:36 +0000

    add responsive navbar

--- static/js/main.js
+++ static/js/main.js
@@ -16,3 +16,11 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
