3cc4320737dfc1f92aee155f1f5f991e6b14110b
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-06-10 09:56:37 +0000

    add admin backend functionality

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from flask_pymongo import PyMongo
 from flask_mail import Mail, Message
 
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from config import mail_username, mail_password
+from functools import wraps
 
 if os.path.exists("env.py"):
     import env
@@ -27,6 +29,114 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 mail = Mail(app)
+
+
+# Login Required Decorator by PalletsProjects (Link in README)
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "user" not in session:
+            flash("You're not authorised to do that.")
+            return redirect(url_for("homepage"))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+# 404 redirect
+@app.errorhandler(404)
+def page_not_found(e):
+
+    return render_template('404.html'), 404
+
+
+# -------------------------ADMIN BITS------------------------ #
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if session.get('user'):
+        return redirect(url_for("login"))
+
+    if request.method == "POST":
+        # checks if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "first_name": request.form.get("first_name"),
+            "last_name": request.form.get("last_name"),
+            "is_admin": False
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into the session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
+        return redirect(url_for("admin", username=session["user"]))
+
+    return render_template("register.html")
+
+
+# Route for user's admin page
+@app.route("/admin/<username>", methods=["GET", "POST"])
+@login_required
+def admin(username):
+
+    # finds session users info from the database and retrieves username
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})
+
+    if session["user"]:
+        return render_template("admin.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+# ---------------------LOGIN AND LOGOUT--------------------- #
+
+
+# Route for member login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        # checks if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                    "admin", username=session["user"]))
+            else:
+                # invalid password
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # invalid username
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+# Route for user logout
+@app.route("/logout")
+def logout():
+
+    # logs user out and removes session cookies
+    flash("You have been logged out")
+    session.pop("user")
+
+    return redirect(url_for("homepage"))
 
 
 # --------------------------HOMEPAGE------------------------- #
@@ -51,7 +161,7 @@ def books():
 @app.route("/books/<book_id>", methods=["GET", "POST"])
 def book(book_id):
     book_id = mongo.db.books.find_one({"_id": ObjectId(book_id)})
-    
+
     return render_template("book_page.html", book_id=book_id)
 
 
@@ -86,11 +196,14 @@ def signup():
 def contact():
 
     if request.method == 'POST':
-        name = request.form.get('name') 
+        name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
 
-        msg = Message(subject=f"Mail from {name}", body=f"Name: {name}\nEmail: {email}\n\nMessage: {message}", sender=mail_username, recipients=['contact@robertclarkauthor.com'])
+        msg = Message(subject=f"Mail from {name}",
+                      body=f"Name: {name}\n Email: {email}\n Msg: {message}",
+                      sender=mail_username,
+                      recipients=['contact@robertclarkauthor.com'])
         mail.send(msg)
         flash("Email sent!")
         return redirect(url_for("homepage"))
